b4da980446a180a1849a0389a28b2fde7a1a39f2
Daniel <imediayala@gmail.com>
2016-05-12 15:59:56 +0200

    first commit

---
+++ AirAppStaff/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AirAppStaff
+//
+//  Created by Daniel on 12/05/16.
+//  Copyright © 2016 idesigndreams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AirAppStaff/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AirAppStaff
+//
+//  Created by Daniel on 12/05/16.
+//  Copyright © 2016 idesigndreams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
